809c7ba24df94db742de72a1b667c728b6afb8b2
Saksham Garg <saksham.garg14509@gmail.com>
2024-09-07 12:33:48 +0530

    Update index.js

--- nullPointers_2.0/index.js
+++ nullPointers_2.0/index.js
@@ -39,7 +39,7 @@ app.use(session({
   secret: '***REMOVED***',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: true }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
